c708c31b1392faedf5141cdec356fb6422492563
Carly <69886350+Aznaia@users.noreply.github.com>
2022-09-18 12:31:01 -0700

    Included Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <Search />
-      <footer>
-        <p>
-          Coded By Carly B on{" "}
-          <a href="https://github.com/Aznaia/weather-app-react">GitHub</a> and
-          hosted by{" "}
-          <a href="https://app.netlify.com/sites/meek-klepon-fc456b/overview">
-            Netlify
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+        <Search />
+        <footer>
+          <p>
+            Coded By Carly B on{" "}
+            <a href="https://github.com/Aznaia/weather-app-react">GitHub</a> and
+            hosted by{" "}
+            <a href="https://app.netlify.com/sites/meek-klepon-fc456b/overview">
+              Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
